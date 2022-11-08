715989f1c351ef0109c6d02c4922342d31160c56
jenya-patel <98290733+jenya-patel@users.noreply.github.com>
2022-11-08 01:59:43 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -12,8 +12,8 @@ window.addEventListener("load", function() {
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video!");
 	video.play();
-	var volume=document.getElementById("slider").value;
-	document.getElementById("volume").innerHTML=volume+"%";
+	var volume=document.getElementById("#slider").value;
+	document.getElementById("#volume").innerHTML=volume+"%";
 });
 
 document.querySelector("#pause").addEventListener("click", function(){
