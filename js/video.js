aab36f8c27f7f8f699ed1d561491b909582822e9
jenya-patel <98290733+jenya-patel@users.noreply.github.com>
2022-11-08 20:32:22 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,15 +1,15 @@
 var video;
 
+// Page Load
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
 	video=document.getElementById("player1");
 	console.log(video);
 	video.autoplay=false;
 	video.loop=false;
-	// video.load();
-
 });
 
+// Play Button
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
 	var volume=document.getElementById("slider").value;
@@ -17,24 +17,27 @@ document.querySelector("#play").addEventListener("click", function() {
 	video.play();
 });
 
+// Pause Button
 document.querySelector("#pause").addEventListener("click", function(){
 	console.log("Pause Video");
 	video.pause();
 });
 
+// Slow Down
 document.querySelector("#slower").addEventListener("click", function(){
 	console.log("Slow Down");
 	video.playbackRate *= 0.9;
 	console.log("video playback rate" + video.playbackRate);
 });
 
+// Speed Up
 document.querySelector("#faster").addEventListener("click", function(){
 	console.log("Speed Up");
 	video.playbackRate /= 0.9;
 	console.log("video playback rate" + video.playbackRate);
 });
 
-// skip ahead
+// Skip Ahead
 document.querySelector("#skip").addEventListener("click", function(){
  	console.log("Skip Ahead");
 	if(video.currentTime + 10 >= video.duration) {
@@ -48,7 +51,7 @@ document.querySelector("#skip").addEventListener("click", function(){
 	}
 });
 
-// mute
+// Mute
 document.querySelector("#mute").addEventListener("click", function(){
 	console.log("Mute");
 	if (video.muted === false) {    
@@ -60,7 +63,8 @@ document.querySelector("#mute").addEventListener("click", function(){
 		document.getElementById("mute").innerHTML= "Mute";
 	};
 });
-// volume slider
+
+// Volume Slider
 document.querySelector("#slider").addEventListener("change", function(){
 	console.log("Change volume", video.volume);
 	video.volume = this.value/100;
@@ -68,12 +72,12 @@ document.querySelector("#slider").addEventListener("change", function(){
 	document.getElementById("volume").innerHTML= video.volume*100+"%";
 });
 
-// styled
+// Styled
 document.querySelector("#vintage").addEventListener("click", function() {
 	video.classList.add('oldSchool');
 });
 
-// original
+// Original
 document.querySelector("#orig").addEventListener("click", function() {
 	video.classList.remove('oldSchool');
 });
